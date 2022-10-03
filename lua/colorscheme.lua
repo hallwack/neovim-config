523d1e842afbd9a596852bf9d0a6f8ffb4f94802
Raihan Adam <hallwack.id@gmail.com>
2022-10-03 23:32:27 +0700

    fix: add colorscheme

--- lua/colorscheme.lua
+++ lua/colorscheme.lua
@@ -1,1 +1,27 @@
-vim.cmd[[ colorscheme minimal ]]
+-- vim.cmd[[ colorscheme minimal ]]
+
+--[[local status, everblush = pcall(require, "everblush")
+if not status then
+	return
+end
+
+local colors = require("everblush.core").get_colors()
+
+everblush.setup({
+	nvim_tree = { contrast = true },
+	-- transparent_background = true,
+	override = {
+		LineNr = {
+			fg = colors.color12,
+		},
+	},
+}) -- or use contrast = false to not apply contrast]]--
+
+vim.cmd[[ colorscheme poimandres ]]
+
+local status, poimandres = pcall(require, "poimandres")
+if not status then
+	return
+end
+
+poimandres.setup {}
