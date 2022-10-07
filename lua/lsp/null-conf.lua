e58c4876141a413475e1c69e2613691624ed8660
Raihan Adam <hallwack.id@gmail.com>
2022-10-07 10:17:08 +0700

    feat: add some formatting and linter

--- lua/lsp/null-conf.lua
+++ lua/lsp/null-conf.lua
@@ -1,9 +1,13 @@
-local status, null_ls = pcall(require, 'null-ls')
-if (not status) then return end
+local status, null_ls = pcall(require, "null-ls")
+if not status then
+	return
+end
 
 null_ls.setup({
-    sources = {
-        null_ls.builtins.formatting.stylua,
-        null_ls.builtins.formatting.prettierd
-    },
+	sources = {
+		null_ls.builtins.formatting.stylua,
+		null_ls.builtins.formatting.prettierd,
+		null_ls.builtins.formatting.blade_formatter,
+		null_ls.builtins.code_actions.eslint_d,
+	},
 })
