386ba5b050ea4f689f2c359d75175e4707a48b2f
Raihan Adam <hallwack.id@gmail.com>
2022-10-07 11:09:19 +0700

    feat: add black formatting for python

--- lua/lsp/null-conf.lua
+++ lua/lsp/null-conf.lua
@@ -8,6 +8,7 @@ null_ls.setup({
 		null_ls.builtins.formatting.stylua,
 		null_ls.builtins.formatting.prettierd,
 		null_ls.builtins.formatting.blade_formatter,
+		null_ls.builtins.formatting.black,
 		null_ls.builtins.code_actions.eslint_d,
 	},
 })
