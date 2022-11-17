87e06712b2e4109b0bfb35fc9ea271709416a419
Raihan Adam <hallwack.id@gmail.com>
2022-11-17 11:33:18 +0700

    feat: add treeseitter configs

---
+++ lua/treesitter.lua
@@ -0,0 +1,10 @@
+local status, treesitter = pcall(require, "nvim-treesitter.configs")
+if not status then
+	return
+end
+
+treesitter.setup({
+	highlight = {
+		enable = true, -- false will disable the whole extension
+	},
+})
