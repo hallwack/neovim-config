35aa0371c63cb42db4074e98db51ad1d129273b5
Raihan Adam <hallwack.id@gmail.com>
2023-01-20 06:54:02 +0700

    fix: add hover handlers for nvim-notify

--- lua/notification.lua
+++ lua/notification.lua
@@ -94,3 +94,19 @@ vim.lsp.handlers["$/progress"] = function(_, result, ctx)
 		notif_data.spinner = nil
 	end
 end
+
+vim.lsp.handlers["textDocument/hover"] = function(_, result, ctx, config)
+	config = config or {}
+	config.focus_id = ctx.method
+	if not (result and result.contents) then
+		--[[ vim.notify("No information available") ]]
+		return
+	end
+	local markdown_lines = vim.lsp.util.convert_input_to_markdown_lines(result.contents)
+	markdown_lines = vim.lsp.util.trim_empty_lines(markdown_lines)
+	if vim.tbl_isempty(markdown_lines) then
+		-- vim.notify('No information available')
+		return
+	end
+	return vim.lsp.util.open_floating_preview(markdown_lines, "markdown", config)
+end
