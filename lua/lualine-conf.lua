4e2ec7abe52a0feccefc8b10018345bdba9880f6
Raihan Adam <hallwack.id@gmail.com>
2023-03-30 17:53:33 +0700

    fix: change lualine theme to auto

--- lua/lualine-conf.lua
+++ lua/lualine-conf.lua
@@ -11,7 +11,7 @@ end
 lualine.setup({
 	options = {
 		icons_enabled = true,
-		theme = "everblush",
+		theme = "auto",
 		component_separators = { left = "", right = "" },
 		section_separators = { left = "", right = "" },
 		disabled_filetypes = { "alpha", "NvimTree", "Outline" },
