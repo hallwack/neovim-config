7ae97d51ac3881de069148e8b656ae72a504cec3
Raihan Adam <hallwack.id@gmail.com>
2023-04-12 01:37:44 +0700

    feat: add keymaps for showing buffers

--- lua/maps.lua
+++ lua/maps.lua
@@ -5,6 +5,7 @@ local cmd = vim.cmd
 map("n", "<C-b>", ":NvimTreeToggle<CR>", default_opts) -- open/close
 map('n', '<leader>ff', "<cmd>lua require('telescope.builtin').find_files()<cr>", default_opts)
 map('n', '<leader>gr', "<cmd>lua require('telescope.builtin').live_grep()<cr>", default_opts)
+map('n', '<leader>bf', "<cmd>lua require('telescope.builtin').buffers()<cr>", default_opts)
 map("i", "jj", "<Esc>", default_opts)
 map("v", "aa", "<Esc>", default_opts)
 map("n", "<C-h>", "<C-w>h", default_opts)
